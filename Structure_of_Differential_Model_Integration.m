2affca4fde6764ddb4add548b52fa5e9b51915d1
kevvvinyao <kevin.yao.cn@yandex.com>
2024-03-31 22:08:24 +0300

    remove additional Temperature

--- Structure_of_Differential_Model_Integration.m
+++ Structure_of_Differential_Model_Integration.m
@@ -1,23 +1,36 @@
 function [] = Structure_of_Differential_Model_Integration( )
     %Set Dynamic Model parameters
-    parameters_of_differential_model = [1.024, % density
-                                        717, % C_v
-                                        10, % Volume
-                                        10, % mass
-                                        300000, % enthalpy input
+    parameters_of_differential_model = [1.8, % density
+                                        717.5, % C_v
+                                        1000, % Volume
+                                        10, % mass  
+                                        1000000, % enthalpy input
                                         293.15, % Temperature input
-                                        400000, % enthalpy output
+                                        3658134, % enthalpy output
                                         393.15, % Temperature output
                                         287, % gas constant for air
-                                        2, % mass flow rate inlet
-                                        1.5]; % mass flow rate outlet
-                                        
+                                        500, % mass flow rate inlet
+                                        800]; % mass flow rate outlet
+
+    % parameters_of_differential_model = [1.024, % density
+    %                                     717, % C_v
+    %                                     10, % Volume
+    %                                     10, % mass
+    %                                     300000, % enthalpy input
+    %                                     293.15, % Temperature input
+    %                                     400000, % enthalpy output
+    %                                     393.15, % Temperature output
+    %                                     287, % gas constant for air
+    %                                     2, % mass flow rate inlet
+    %                                     1.5]; % mass flow rate outlet
+                       
+    
     %Integrator Options
     options_ode = odeset('RelTol', 1e-3, 'AbsTol', 1e-6);
 
     %Initial conditions for vector of the integration variables - SI unit
     pressure = 101325;
-    Temperature = 293.15;
+    Temperature = 298;
     initial_cond_for_differential_model = [pressure; Temperature];
 
     t_start=0.0;
@@ -47,7 +60,7 @@ function [dY_dt] = differential_model(time, Y, parameters_of_differential_model)
     % 这里需要定义相关的参数
     % 例如density, C_v, Volume, mass, enthalpy_input, Temperature_input, enthalpy_output, Temperature_output, R, mass_inlet, mass_outlet
 
-    dTemperature = (1 / (density * C_v * Volume)) * (mass * enthalpy_input * Temperature_input - mass * enthalpy_output * Temperature_output);
+    dTemperature = (1 / (density * C_v * Volume)) * (mass_inlet * enthalpy_input - mass_outlet * enthalpy_output);
     dpressure = (R * Temperature / Volume) * (mass_inlet - mass_outlet) + pressure / Temperature * dTemperature; 
 
     dY_dt = [dpressure; dTemperature];
